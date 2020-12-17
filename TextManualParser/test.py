35c39a8229a5827673b09766232d2039962ee395
MarioPalazuelos <m.pala.arg@gmail.com>
2020-12-17 10:07:46 -0600

    Tagger Function added

--- TextManualParser/test.py
+++ TextManualParser/test.py
@@ -11,23 +11,25 @@ from text_parser import TextParser
 
 
 df = pd.read_csv("test_text.txt", header=None, names=['Gina'])
-i = 1
+i = 0
 column = 'Gina'
 
 
-
-dft = TextParser(df, i, column, 'Tag')
-
-
-
+# Prueba Forward y Backward
 check = TextParser(df,i, column, 'Tag')
-print(check.CELL)
-# check = TextParser(df,i, column).Forward()
-check.Forward()
-print(check.CELL)
+# print(check.CELL)
+# # check = TextParser(df,i, column).Forward()
+# check.Forward()
+# print(check.CELL)
 
-check.Backward()
-print(check.CELL)
+# check.Backward()
+# print(check.CELL)
 
-check.Backward()
-print(check.CELL)
+# check.Backward()
+# print(check.CELL)
+
+# #Prueba Tagger
+# check.Tagger()
+# check.Forward()
+# check.Tagger()
+# check.Forward()
--- TextManualParser/text_parser.py
+++ TextManualParser/text_parser.py
@@ -2,36 +2,39 @@
 """
 Created on Wed Dec 16 16:55:30 2020
 
-@author: mpala
+@author: Mario Palazuelos - Aliado Analítico, Morelos, México
 """
 
 class TextParser:
     
     def __init__(self, DATAFRAME, INDEX, COLUMN, TAG):
         
-        #Creating the variables and methods that will build the Class Skeleton.
+        #Creating the Objects that will build the Class Skeleton.
         self.DATAFRAME = DATAFRAME
         self.INDEX = INDEX
         self.COLUMN = COLUMN
         self.TAG = TAG
         
-        #Create the "TAG" column to use later with the Tagger Function
+        #Create the "TAG" Column Object to use later with the Tagger Function
         self.DATAFRAME[TAG] = 0
         
         #Creating the Cell Object to impute functions
         self.CELL = self.DATAFRAME.loc[self.INDEX, self.COLUMN]
         
 
-
-   
+        #Forward function forwards to the next cell from current INDEX
     def Forward(self):
         self.INDEX = self.INDEX + 1
         self.CELL = self.DATAFRAME.loc[self.INDEX, self.COLUMN]
         
+        
+        #Backward function goes back to previous cell from current INDEX
     def Backward(self):
         self.INDEX = self.INDEX - 1
         self.CELL = self.DATAFRAME.loc[self.INDEX, self.COLUMN]
         
         
-        
+        #Tagger function tags the current cell with the users input
+    def Tagger(self):
+        self.DATAFRAME.loc[self.INDEX, self.TAG] = input()
         
