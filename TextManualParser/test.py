e82c66471f9f344e3642b483bfe0057647acf02a
MarioPalazuelos <m.pala.arg@gmail.com>
2020-12-16 20:00:59 -0600

    Forward and Backward Functions

--- TextManualParser/test.py
+++ TextManualParser/test.py
@@ -10,6 +10,18 @@ import pandas as pd
 from text_parser import TextParser
 
 
-df = pd.read_csv("test_text.txt", header=None)
+df = pd.read_csv("test_text.txt", header=None, names=['Gina'])
 i = 1
-check = TextParser(df,i)
+column = 'Gina'
+
+check = TextParser(df,i, column)
+print(check.CELL)
+# check = TextParser(df,i, column).Forward()
+check.Forward()
+print(check.CELL)
+
+check.Backward()
+print(check.CELL)
+
+check.Backward()
+print(check.CELL)
--- TextManualParser/text_parser.py
+++ TextManualParser/text_parser.py
@@ -7,11 +7,23 @@ Created on Wed Dec 16 16:55:30 2020
 
 class TextParser:
     
-    def __init__(self, DATAFRAME, INDEX):
+    def __init__(self, DATAFRAME, INDEX, COLUMN):
         self.DATAFRAME = DATAFRAME
         self.INDEX = INDEX
-        
+        self.COLUMN = COLUMN
+        self.CELL = self.DATAFRAME.loc[self.INDEX, self.COLUMN]
     
+
+
+   
     def Forward(self):
-        return None
+        self.INDEX = self.INDEX + 1
+        self.CELL = self.DATAFRAME.loc[self.INDEX, self.COLUMN]
+        
+    def Backward(self):
+        self.INDEX = self.INDEX - 1
+        self.CELL = self.DATAFRAME.loc[self.INDEX, self.COLUMN]
+        
+        
+        
         
